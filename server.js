a97f47a0ce76f334e325ccf61348a6b46604013c
odiljon1334 <odil1334@gmail.com>
2024-08-14 02:46:01 +0500

    feat: prepare harid ejs page

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.set("views", "views")
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/", function(req, res) {
-    res.end("<h1>Hello World!</h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "succes"});
 });
-app.get("/gift", function(req, res) {
-    res.end("<h1>Siz sovg'alar bo'limida siz!</h1>");
+
+app.get("/", function(req, res) {
+    res.render('harid');
 });
 
 const server = http.createServer(app);
