4e1dfb34e76f7bbf38784dd44d2ec0e6b8700460
odiljon1334 <odil1334@gmail.com>
2024-09-02 23:55:56 +0500

    feat:  TypeScript va Patternlar & MITASK-G

--- train.js
+++ train.js
@@ -1,10 +1,32 @@
+ // G-TASK
+ // Define
+ function getHighestIndex(arr) {
+    let index = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        // agar arraymizni  [i]nchi indexi katta bo'lsa arrayimizni indexdan
+        // u katta emas for loop ishga tushadi.
+        // endi arrayimizni [i]nchi indexi 1ga teng u katta emas.
+        // for loop ishga tushmedi.  
+        if(arr[i] > arr[index]) {
+            // yuqorida xosil qilib olgan o'zgaruvchimizni qiymati 0 ga tenglab olgandik
+            // endi [i]ning qiymati 1 ga teng indeximizni qiymatini i ga tenglab olib uni return qilib olamiz.
+            index = i;
+        }
+    }
+    return index;
+ }
+ // Call
+ const result = getHighestIndex([5, 21, 12, 21, 8]);
+ console.log('G-TASK:', result);
+
 // F-TASK
-function findDoublers(str) {
-    return new Set(str).size !== str.length;
-}
-// Call
-const result = findDoublers('hello');
-console.log('F-TASK:', result);
+// function findDoublers(str) {
+//     return new Set(str).size !== str.length;
+// }
+// // Call
+// const result = findDoublers('hello');
+// console.log('F-TASK:', result);
 
 // // E-TASK
 // const moment = require("moment");
