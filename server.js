55c94d4df9e42f668bccf4a7051ecd4a97aac322
odiljon1334 <odiljon9507@gmail.com>
2025-05-14 03:45:14 +0500

    feat: create Dockerfile to generete images

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://odil1334:uqud1j0nTfDpp1IN@cluster0.bpbgp.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+const connectionString =
+  "mongodb+srv://odil1334:uqud1j0nTfDpp1IN@cluster0.bpbgp.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
 
-mongodb.connect(connectionString, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}, (err, client) => {
-    if(err) console.log("ERROR on connection MongoDB");
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
     else {
-        console.log("MongoDB connection succeed");
-        module.exports = client;
-        const app = require("./app");
-        const server = http.createServer(app);
-        let PORT = process.env.PORT || 6010;
-        server.listen(PORT, function() {
-            console.log(
-                `Server is running on port ${PORT}, http://localhost:${PORT}`);
-});
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      // Boshqa kodlar...
+      let PORT = process.env.PORT || 3003;
+      server.listen(PORT, function () {
+        console.log(
+          `Server is running on port ${PORT}, http://localhost:${PORT}`
+        );
+      });
+      // Boshqa kodlar...
     }
-});
+  }
+);
--- train.js
+++ train.js
@@ -1,24 +1,64 @@
- // G-TASK
- // Define
- function getHighestIndex(arr) {
-    let index = 0;
-
-    for(let i = 0; i < arr.length; i++){
-        // agar arraymizni  [i]nchi indexi katta bo'lsa arrayimizni indexdan
-        // u katta emas for loop ishga tushadi.
-        // endi arrayimizni [i]nchi indexi 1ga teng u katta emas.
-        // for loop ishga tushmedi.  
-        if(arr[i] > arr[index]) {
-            // yuqorida xosil qilib olgan o'zgaruvchimizni qiymati 0 ga tenglab olgandik
-            // endi [i]ning qiymati 1 ga teng indeximizni qiymatini i ga tenglab olib uni return qilib olamiz.
-            index = i;
+function removeItem(item, removeable) {
+    const updated = [];
+    for(let i = 0; i < item.length; i++) {
+        if (item[i] !== removeable) {
+            updated.push(item[i]);
         }
     }
-    return index;
- }
- // Call
- const result = getHighestIndex([5, 21, 12, 21, 8]);
- console.log('G-TASK:', result);
+    return updated
+}
+
+
+// const item = {
+//     inventory: 5,        // oddiy narx uchun inventar soni
+//     price: 100,           // oddiy narx
+//     salePrice: 10,        // aksiya narxi
+//     saleInventory: 2      // aksiya inventar soni
+// };
+ 
+//  function getLowestPrice(item) {
+//     let count = item.inventory;
+//     let price = item.price;
+    
+//     if (item.salePrice) {
+//         let count = item.saleInventory;
+        
+//         if (count > 0) {
+//             price = item.salePrice;
+//         }
+//     }
+//     if (count) {
+//         return price;
+//     }
+//     return 0;
+//  }
+//  // Funksiyani chaqiramiz
+//  const lowestPrice = getLowestPrice(item);
+
+// // Natijani konsolga chiqaramiz
+// console.log(`Eng arzon narx: $${lowestPrice}`);
+ 
+ // G-TASK
+ // Define
+//  function getHighestIndex(arr) {
+//     let index = 0;
+
+//     for(let i = 0; i < arr.length; i++){
+//         // agar arraymizni  [i]nchi indexi katta bo'lsa arrayimizni indexdan
+//         // u katta emas for loop ishga tushadi.
+//         // endi arrayimizni [i]nchi indexi 1ga teng u katta emas.
+//         // for loop ishga tushmedi.  
+//         if(arr[i] > arr[index]) {
+//             // yuqorida xosil qilib olgan o'zgaruvchimizni qiymati 0 ga tenglab olgandik
+//             // endi [i]ning qiymati 1 ga teng indeximizni qiymatini i ga tenglab olib uni return qilib olamiz.
+//             index = i;
+//         }
+//     }
+//     return index;
+//  }
+//  // Call
+//  const result = getHighestIndex([5, 21, 12, 21, 8]);
+//  console.log('G-TASK:', result);
 
 // F-TASK
 // function findDoublers(str) {
